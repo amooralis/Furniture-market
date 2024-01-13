610e790a2b4fbe89ec0f6204f5da05f4d03bb038
amooralis <71789031+amooralis@users.noreply.github.com>
2024-01-13 11:34:53 +0300

    Add files via upload

---
+++ microservices/ProductService/product_app/db/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@postgres-product:5434/products"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+AsyncSessionLocal = async_sessionmaker(engine, autocommit=False, autoflush=False, class_=AsyncSession)
+
+Base = declarative_base()
+
+
+
